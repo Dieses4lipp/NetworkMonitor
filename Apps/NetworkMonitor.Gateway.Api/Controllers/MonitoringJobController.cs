2db98b84677c95e481b0c802c0578e3bec2c0dd1
Dieses4lipp <154359162+Dieses4lipp@users.noreply.github.com>
2026-02-27 09:56:36 +0100

    added the endpoints

---
+++ Apps/NetworkMonitor.Gateway.Api/Controllers/MonitoringJobController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetworkMonitor.Gateway.Api.Controllers
+{
+    [ApiController]
+    [Route("jobs")]
+    public class MonitoringJobController : ControllerBase
+    {
+        [HttpPost]
+        public void CreateNewJob()
+        {
+            Console.WriteLine("Create New Job hit");
+        }
+
+        [HttpPatch]
+        [Route("{id}")]
+        public void UpdateJobById([FromRoute] Guid id)
+        {
+            Console.WriteLine($"Update Job by Id hit with {id}");
+        }
+        [HttpGet]
+        [Route("types")]
+        public void GetJobTypes()
+        {
+            Console.WriteLine("Get Job Types hit");
+        }
+    }
+}
